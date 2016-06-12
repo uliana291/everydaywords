75fea5fde527299c687dcbdaeb47474cf8f8e717
Julia <uliana291@gmail.com>
2016-06-12 16:24:38 +0200

    modified translations api

---
+++ app/controllers/trainings_controller.rb
@@ -0,0 +1,4 @@
+class TrainingsController < ApiController
+
+
+end
--- app/controllers/translation_controller.rb
+++ app/controllers/translation_controller.rb
@@ -3,26 +3,35 @@ class TranslationController < ApiController
 
   def list_user
     fullTranslation = []
-    uTranslations = current_user.translations
-    uTranslations.each do |t|
+    uTranslations = current_user.user_translations
+    uTranslations.each do |uTranslation|
+      t = uTranslation.translation
       elOriginal = TextElement.find_by(id: t.original_id)
       elTranslation = TextElement.find_by(id: t.translated_one_id)
 #      uContextTexts = current_user.context_texts.pluck(:id)
+      contextTexts = []
       if params[:context_text_id]
-        trContextText = TranslationInContextText.where(translation_id: t.id).where(context_text_id: params[:context_text_id])
+        trContextText = TranslationInContextText.where(translation_id: t.id).where(user_id: current_user.id).where(context_text_id: params[:context_text_id])
+        contextTexts = [{'context_text_id' => trContextText.context_text_id,
+                         'position' => trContextText.position,
+                         'selection_length' => trContextText.selection_length}]
       else
-        trContextText = TranslationInContextText.where(translation_id: t.id)
+        trContextText = TranslationInContextText.where(translation_id: t.id).where(user_id: current_user.id)
+        trContextText.each do |tr|
+          contextTexts.push('context_text_id' => tr.context_text_id,
+                            'position' => tr.position,
+                            'selection_length' => tr.selection_length)
+        end
       end
-      trContextText.each do |tr|
-        fullTranslation.push('lang_from_id' => elOriginal.language_id,
+      fullTranslation.push('lang_from_id' => elOriginal.language_id,
                            'lang_to_id' => elTranslation.language_id,
                            'original' => elOriginal.value,
                            'translated_one' => elTranslation.value,
                            'part_of_speech' => elOriginal.part_of_speech,
-                           'context_text_id' => tr.context_text_id,
-                           'position' => tr.position,
-                           'selection_length' => tr.selection_length)
-      end
+                           'learning_stage' => uTranslation.learning_stage,
+                           'next_training_at' => uTranslation.next_training_at,
+                           'training_history' => uTranslation.training_history,
+                           'context_texts' => contextTexts)
     end
     render(json: fullTranslation)
   end
@@ -33,22 +42,29 @@ class TranslationController < ApiController
     if lOriginal && lTranslation
       ct = ContextText.find_by(id: params[:context_text_id])
       if ct
-        elOriginal = TextElement.create(value: params[:original],
-                                        language_id: lOriginal.id, part_of_speech: params[:part_of_speech])
-        elTranslation = TextElement.create(value: params[:translated_one],
-                                           language_id: lTranslation.id, part_of_speech: params[:part_of_speech])
+        elOriginal = TextElement.find_or_create_by(value: params[:original],
+                                                   language_id: lOriginal.id, part_of_speech: params[:part_of_speech])
+        elTranslation = TextElement.find_or_create_by(value: params[:translated_one],
+                                                      language_id: lTranslation.id, part_of_speech: params[:part_of_speech])
         if elOriginal && elTranslation
-          t = Translation.create(original_id: elOriginal.id, translated_one_id: elTranslation.id)
+          t = Translation.find_or_create_by(original_id: elOriginal.id, translated_one_id: elTranslation.id)
           if t
-            tContextText = TranslationInContextText.create(position: params[:position],
-                                                           selection_length: params[:selection_length],
-                                                           translation_id: t.id,
-                                                           context_text_id: params[:context_text_id],
-                                                           user_id: current_user.id)
+            tContextText = TranslationInContextText.find_or_create_by(position: params[:position],
+                                                                      selection_length: params[:selection_length],
+                                                                      translation_id: t.id,
+                                                                      context_text_id: params[:context_text_id],
+                                                                      user_id: current_user.id)
             if tContextText
-              current_user.translations<<t
-              render :json => {:result => { 'text_element_from_id' => elOriginal.id,
-                                            'text_element_to_id' => elTranslation.id } }, :status => 200
+              d = Date.today
+              uTranslation = current_user.user_translations.find_or_create_by(translation: t)
+              uTranslation.update_attributes(learning_stage: '1', next_training_at: (d+1).to_s,
+                                             training_history: [{when: d.to_s, next_stage: '1'}].to_s)
+              if uTranslation
+                render :json => {:result => {'text_element_from_id' => elOriginal.id,
+                                             'text_element_to_id' => elTranslation.id}}, :status => 200
+              else
+                render :json => {:error => 'internal-server-error'}, :status => 500
+              end
             else
               render :json => {:error => 'internal-server-error'}, :status => 500
             end
