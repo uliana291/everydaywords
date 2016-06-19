245cbdcb38ba3d5b4f0c807d93aae835d87559c7
Julia <uliana291@gmail.com>
2016-06-19 22:00:19 +0200

    daily_finish

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -6,44 +6,19 @@ require 'json'
     uTranslation = UserTranslation.find(params[:user_translation_id])
     if uTranslation
       currentStage = uTranslation.learning_stage
-      newStage = ''
       if params[:answer_result].eql? 'passed'
-        case currentStage
-          when '1'
-            newStage = '2'
-          when '2'
-            newStage = '7'
-          when '7'
-            newStage = '14'
-          when '14'
-            newStage = '30'
-          when '30'
-            newStage = 'finished'
-        end
+        newStage = to_next_stage(currentStage)
         uTranslation.learning_stage = newStage
       else
         newStage = '1'
         uTranslation.learning_stage = '1'
-      end
-      date = Date.today
-      case newStage
-        when '1','2'
-          newNextTraining = (date+1).to_s
-        when '7'
-          newNextTraining = (date+7).to_s
-        when '14'
-          newNextTraining = (date+14).to_s
-        when '30'
-          newNextTraining = (date+30).to_s
-        when 'finished'
-          newNextTraining = null
       end
       time = DateTime.now.to_s
       training_history = [{'when' => time,
                            'previous_stage' => currentStage,
                            'new_stage' => newStage,
                            'training_state' => params[:answer_result]}]
-      uTranslation.next_training_at = newNextTraining
+      uTranslation.next_training_at = get_next_training_time(newStage)
       uTranslation.training_history = training_history.to_s
       if uTranslation.save
         render :json => {:status => 'ok'}, :status => 200
@@ -125,11 +100,112 @@ require 'json'
       json_data[:training_history].each do |el|
         training_history.push(el[:results])
       end
-
-
+      training_history.flatten!
+      tId = {}
+      training_history.each do |h|
+        tId[h[:user_translation_id]] ||= []
+        tId[h[:user_translation_id]] << h[:answer]
+      end
+      tId.each do |translation, results|
+        uTranslation = UserTranslation.find(translation)
+        training_count = results.length
+        training_passed = 0
+        result.each do |r|
+          if r == 'passed'
+            training_passed += 1
+          end
+        end
+        percent = training_passed.to_f/training_count.to_f
+        if percent == 1
+          newStage = to_next_stage(uTranslation.learning_stage)
+        elsif percent < 0.8 && percent > 0.6
+          newStage = minus_level(uTranslation.learning_stage)
+        elsif percent == 0.5
+          newStage = minus_two(uTranslation.learning_stage)
+        else
+          newStage = '1'
+        end
+        time = DateTime.now.to_s
+        training_history = [{'when' => time,
+                             'previous_stage' => uTranslation.learning_stage,
+                             'new_stage' => newStage,
+                             'training_state' => (newStage == 'passed'? 'passed' : 'dont_know')}]
+        uTranslation.learning_stage = newStage
+        uTranslation.training_history = training_history.to_s
+        uTranslation.next_training_at = (percent == 1? get_next_training_time(uTranslation.learning_stage) : (Date.today+1).to_s)
+        uTranslation.save
+        if uTranslation.save
+          render :json => {:status => 'ok'}, :status => 200
+        else
+          render :json => {:error => 'internal-server-error'}, :status => 500
+        end
+      end
     end
   end
 
+
+  private
+
+  def to_next_stage(currentStage)
+    case currentStage
+      when '1'
+        newStage = '2'
+      when '2'
+        newStage = '7'
+      when '7'
+        newStage = '14'
+      when '14'
+        newStage = '30'
+      when '30'
+        newStage = 'finished'
+    end
+    newStage
+  end
+
+  def minus_level(currentStage)
+    case currentStage
+      when '1','2'
+        newStage = '1'
+      when '7'
+        newStage = '2'
+      when '14'
+        newStage = '7'
+      when '30'
+        newStage = '14'
+    end
+    newStage
+  end
+
+
+  def minus_two(currentStage)
+    case currentStage
+      when '1','2','7'
+        newStage = '1'
+      when '14'
+        newStage = '2'
+      when '30'
+        newStage = '7'
+    end
+    newStage
+  end
+
+
+  def get_next_training_time(newStage)
+    date = Date.today
+    case newStage
+      when '1','2'
+        newNextTraining = (date+1).to_s
+      when '7'
+        newNextTraining = (date+7).to_s
+      when '14'
+        newNextTraining = (date+14).to_s
+      when '30'
+        newNextTraining = (date+30).to_s
+      when 'finished'
+        newNextTraining = null
+    end
+    newNextTraining
+  end
 
 
 end
