9a30d7bc49e1e8b5e47b00e6c786aec1e55a45a3
Julia <uliana291@gmail.com>
2016-08-02 00:18:54 +0300

    cors fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,6 +7,26 @@ class ApiController < ActionController::Metal
   include ActionController::StrongParameters
 
   before_action :authorize_touch
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
 
   def ping
     render :json => {:result => { 'response' => 'pong' }}, :status => 200
