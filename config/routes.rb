5e3722ea9e66de2a4c1e56dfc96b26c7ff89687b
Julia <uliana291@gmail.com>
2016-11-12 22:41:09 +0100

    more beauty

--- config/routes.rb
+++ config/routes.rb
@@ -12,29 +12,41 @@ Rails.application.routes.draw do
   end
 
   scope '/api' do
+
+    scope '/user' do
+      get '/list', to: 'user#list'
+      get '/switch/:id', to: 'user#become'
+      get '/profile/', to: 'user#show'
+      get '/:id/profile', to: 'user#show'
+      match '/profile', to: 'user#update', via: :patch
+      get '/current', to: 'user#current'
+    end
+
+    resources :context_text, only: [ :create, :show, :update, :destroy]
+    scope '/context_text' do
+      get '/list', to: 'context_text#list'
+      get '/list/user', to: 'context_text#list_user'
+      get '/list_by_url', to: 'context_text#url_list'
+    end
+
+    scope '/trainings' do
+      match '/daily', to: 'trainings#add', via: :post
+      get '/daily/list', to: 'trainings#list'
+      get '/daily/:id', to: 'trainings#get'
+      match '/daily/:id', to: 'trainings#destroy', via: :delete
+      get '/daily/:id/finish', to: 'trainings#finish_daily'
+      match '/daily/:id', to: 'trainings#update', via: :patch
+      get '/get', to: 'translation#list_translations'
+    end
+
     get '/language/list', to: 'language#list'
     resources :language, only: [ :show, :create, :destroy, :update]
-    get '/context_text/list', to: 'context_text#list'
-    get '/context_text/list/user', to: 'context_text#list_user'
-    get '/context_text/list_by_url', to: 'context_text#url_list'
+
     get '/translation/list/user', to: 'translation#list_user'
     match '/translation/:id', to: 'translation#destroy', via: :delete
-    get '/trainings/get', to: 'translation#list_translations'
-    resources :context_text, only: [ :create, :show, :update, :destroy]
     match '/add_translation', to: 'translation#add', via: :post
-    match '/trainings/daily', to: 'trainings#add', via: :post
-    get '/trainings/daily/list', to: 'trainings#list'
-    get '/trainings/daily/:id', to: 'trainings#get'
-    match '/trainings/daily/:id', to: 'trainings#destroy', via: :delete
-    get '/trainings/daily/:id/finish', to: 'trainings#finish_daily'
-    match '/trainings/daily/:id', to: 'trainings#update', via: :patch
-    get '/user/current', to: 'user#current'
+
     get '/ping', to: 'api#ping'
-    get '/user/list', to: 'user#list'
-    get '/user/switch/:id', to: 'user#become'
-    get '/user/profile/', to: 'user#show'
-    get '/user/:id/profile', to: 'user#show'
-    match '/user/profile', to: 'user#update', via: :patch
   end
 
   get '/login' => 'welcome#index'
